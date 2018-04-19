e28e8628a02de814f8c0d7be76b30f2f99a1531d
Lukas Bosse <LukasBosse95@gmail.com>
2018-04-19 20:04:02 +0200

    Inital commit

---
+++ assets/Kartenmaterial/Tilesets/EnergyWorld_Tileset_Size_1.tsx
@@ -0,0 +1,64 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="EnergyWorld_Tileset_Size_1" tilewidth="525" tileheight="299" tilecount="20" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="525" height="297" source="../Assets/EON_EnergyCity_Bodentexturen_Wiese.png"/>
+ </tile>
+ <tile id="1">
+  <image width="524" height="298" source="../Assets/EON_EnergyCity_Bodentexturen_Asphalt.png"/>
+ </tile>
+ <tile id="2">
+  <image width="524" height="281" source="../Assets/EON_EnergyCity_Bodentexturen_Meer.png"/>
+ </tile>
+ <tile id="3">
+  <image width="524" height="297" source="../Assets/EON_EnergyCity_Bodentexturen_Sand.png"/>
+ </tile>
+ <tile id="4">
+  <image width="524" height="297" source="../Assets/EON_EnergyCity_Bodentexturen_See - 1.png"/>
+ </tile>
+ <tile id="5">
+  <image width="524" height="298" source="../Assets/EON_EnergyCity_Bodentexturen_See - 2.png"/>
+ </tile>
+ <tile id="6">
+  <image width="524" height="299" source="../Assets/EON_EnergyCity_Bodentexturen_See - 3.png"/>
+ </tile>
+ <tile id="7">
+  <image width="524" height="298" source="../Assets/EON_EnergyCity_Bodentexturen_See - 4.png"/>
+ </tile>
+ <tile id="8">
+  <image width="524" height="297" source="../Assets/EON_EnergyCity_Bodentexturen_See - 5.png"/>
+ </tile>
+ <tile id="9">
+  <image width="525" height="282" source="../Assets/EON_EnergyCity_Bodentexturen_See - 6.png"/>
+ </tile>
+ <tile id="10">
+  <image width="524" height="282" source="../Assets/EON_EnergyCity_Bodentexturen_See - 7.png"/>
+ </tile>
+ <tile id="11">
+  <image width="524" height="282" source="../Assets/EON_EnergyCity_Bodentexturen_See - 8.png"/>
+ </tile>
+ <tile id="12">
+  <image width="524" height="282" source="../Assets/EON_EnergyCity_Bodentexturen_See.png"/>
+ </tile>
+ <tile id="13">
+  <image width="524" height="297" source="../Assets/EON_EnergyCity_Bodentexturen_Strasse - Gerade 1.png"/>
+ </tile>
+ <tile id="14">
+  <image width="524" height="298" source="../Assets/EON_EnergyCity_Bodentexturen_Strasse - Gerade 2.png"/>
+ </tile>
+ <tile id="15">
+  <image width="524" height="298" source="../Assets/EON_EnergyCity_Bodentexturen_Strasse - Kreuzung.png"/>
+ </tile>
+ <tile id="16">
+  <image width="524" height="297" source="../Assets/EON_EnergyCity_Bodentexturen_Strasse - Kurve 1.png"/>
+ </tile>
+ <tile id="17">
+  <image width="524" height="298" source="../Assets/EON_EnergyCity_Bodentexturen_Strasse - Kurve 2.png"/>
+ </tile>
+ <tile id="18">
+  <image width="524" height="298" source="../Assets/EON_EnergyCity_Bodentexturen_Strasse - Kurve 3.png"/>
+ </tile>
+ <tile id="19">
+  <image width="524" height="298" source="../Assets/EON_EnergyCity_Bodentexturen_Strasse - Kurve 4.png"/>
+ </tile>
+</tileset>
---
+++ assets/Kartenmaterial/Tilesets/EnergyWorld_Tileset_Size_3_to_7.tsx
@@ -0,0 +1,124 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="EnergyWorld_Tileset_Size_3_to_7" tilewidth="3689" tileheight="2573" tilecount="40" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="1578" height="1633" source="../Assets/Kernkraftwerk_Size3_Baustufe.png"/>
+ </tile>
+ <tile id="1">
+  <image width="3688" height="2573" source="../Assets/Solaranlage_Size7.png"/>
+ </tile>
+ <tile id="2">
+  <image width="3689" height="2573" source="../Assets/Solaranlage_Size7_Baustufe.png"/>
+ </tile>
+ <tile id="3">
+  <image width="3141" height="1868" source="../Assets/Windpark_klein_Offshore_Size6.png"/>
+ </tile>
+ <tile id="4">
+  <image width="3141" height="1868" source="../Assets/Windpark_klein_Offshore_Size6_Bauvorstufe.png"/>
+ </tile>
+ <tile id="5">
+  <image width="3141" height="1868" source="../Assets/Windpark_klein_Onshore_Size6.png"/>
+ </tile>
+ <tile id="6">
+  <image width="3141" height="1868" source="../Assets/Windpark_klein_Onshore_Size6_Bauvorstufe.png"/>
+ </tile>
+ <tile id="7">
+  <image width="1578" height="1633" source="../Assets/Kernkraftwerk_Size3.png"/>
+ </tile>
+ <tile id="8">
+  <image width="1049" height="824" source="../Assets/Baum_Gross_4.png"/>
+ </tile>
+ <tile id="9">
+  <image width="1049" height="824" source="../Assets/Baum_Gross_2.png"/>
+ </tile>
+ <tile id="10">
+  <image width="1049" height="824" source="../Assets/Baum_Gross_3.png"/>
+ </tile>
+ <tile id="11">
+  <image width="1049" height="824" source="../Assets/Baum_Gross_4.png"/>
+ </tile>
+ <tile id="12">
+  <image width="1049" height="824" source="../Assets/Baum_Gross_5.png"/>
+ </tile>
+ <tile id="13">
+  <image width="1049" height="824" source="../Assets/Haus_Alt_Haus_01_mit_Hintergrund4 _1.png"/>
+ </tile>
+ <tile id="14">
+  <image width="1049" height="824" source="../Assets/Haus_Alt_Haus_02_ohne_Hintergrund4_2.png"/>
+ </tile>
+ <tile id="15">
+  <image width="1049" height="824" source="../Assets/Haus_Alt_Haus_03_mit_Hintergrund4 _2.png"/>
+ </tile>
+ <tile id="16">
+  <image width="1049" height="824" source="../Assets/Haus_Modern_Haus_02_mit_Hintergrund4_2.png"/>
+ </tile>
+ <tile id="17">
+  <image width="1050" height="824" source="../Assets/Haus_Modern_Haus_03_ohne_Hintergrund4_1.png"/>
+ </tile>
+ <tile id="18">
+  <image width="1050" height="824" source="../Assets/Haus_Modern_Haus_03_ohne_Hintergrund4_2.png"/>
+ </tile>
+ <tile id="19">
+  <image width="1049" height="824" source="../Assets/Haus_Alt_Haus_01_mit_Hintergrund4 _1.png"/>
+ </tile>
+ <tile id="20">
+  <image width="1049" height="824" source="../Assets/Haus_Alt_Haus_02_ohne_Hintergrund4 (1).png"/>
+ </tile>
+ <tile id="21">
+  <image width="1049" height="824" source="../Assets/Haus_Alt_Haus_02_ohne_Hintergrund4_2.png"/>
+ </tile>
+ <tile id="22">
+  <image width="1049" height="824" source="../Assets/Haus_Alt_Haus_03_mit_Hintergrund4_1.png"/>
+ </tile>
+ <tile id="23">
+  <image width="1049" height="824" source="../Assets/Haus_Alt_Haus_03_mit_Hintergrund4 _2.png"/>
+ </tile>
+ <tile id="24">
+  <image width="1049" height="824" source="../Assets/Haus_Alt_Haus_04_ohne_Hintergrund4 _1.png"/>
+ </tile>
+ <tile id="25">
+  <image width="1049" height="824" source="../Assets/Haus_Alt_Haus_04_ohne_Hintergrund4_2.png"/>
+ </tile>
+ <tile id="26">
+  <image width="1049" height="824" source="../Assets/Haus_Alt_Haus_05_mit_Hintergrund4 _1.png"/>
+ </tile>
+ <tile id="27">
+  <image width="1049" height="824" source="../Assets/Haus_Alt_Haus_05_mit_Hintergrund4 _2.png"/>
+ </tile>
+ <tile id="28">
+  <image width="1049" height="824" source="../Assets/Haus_Modern_Haus_01_ohne_Hintergrund4_1.png"/>
+ </tile>
+ <tile id="29">
+  <image width="3141" height="1868" source="../Assets/Wald_gross_01_mit_Hintergrund6 (1).png"/>
+ </tile>
+ <tile id="30">
+  <image width="3141" height="1868" source="../Assets/Wald_gross_01_mit_Hintergrund6 (2).png"/>
+ </tile>
+ <tile id="31">
+  <image width="3141" height="1868" source="../Assets/Wald_gross_01_ohne_Hintergrund6 (1).png"/>
+ </tile>
+ <tile id="32">
+  <image width="3141" height="1868" source="../Assets/Wald_gross_01_ohne_Hintergrund6 (2).png"/>
+ </tile>
+ <tile id="33">
+  <image width="1049" height="824" source="../Assets/Wald_Gross_mit_Hintergrund4 (1).png"/>
+ </tile>
+ <tile id="34">
+  <image width="1049" height="824" source="../Assets/Wald_Gross_mit_Hintergrund4 (2).png"/>
+ </tile>
+ <tile id="35">
+  <image width="1049" height="824" source="../Assets/Wald_Gross_ohne_Hintergrund4 (1).png"/>
+ </tile>
+ <tile id="36">
+  <image width="1050" height="824" source="../Assets/Wald_Gross_ohne_Hintergrund4 (2).png"/>
+ </tile>
+ <tile id="37">
+  <image width="1049" height="824" source="../Assets/Wald_klein_ohne_Hintergrund4.png"/>
+ </tile>
+ <tile id="38">
+  <image width="1049" height="824" source="../Assets/Wald_mittel_mit_Hintergrund4.png"/>
+ </tile>
+ <tile id="39">
+  <image width="1049" height="824" source="../Assets/Wald_mittel_ohne_Hintergrund4.png"/>
+ </tile>
+</tileset>
